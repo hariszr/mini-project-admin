4e937fce8c6f24d23765b335a62b6a7c82fa341c
Haris <haris@MacBook-Air.local>
2026-01-06 01:26:30 +0700

    success connect with GET API 1-2 pages in reqres for List User

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -2,22 +2,28 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET() {
   try {
-    const res = await fetch("https://reqres.in/api/users?page=1", {
-      headers: {
-        "x-api-key": process.env.REQRES_API_KEY!
-      },
+    const pages = [1, 2, 3];
+    const requests = pages.map((page) =>
+    
+    fetch(`https://reqres.in/api/users?page=${page}`, {
+        headers: {
+          "x-api-key": process.env.REQRES_API_KEY!,
+        },
+      }).then((res) => {
+        if (!res.ok) {
+          throw new Error(`Reqres error ${res.status}`);
+        }
+        return res.json();
+      }));
+
+    const results = await Promise.all(requests);
+
+    const users = results.flatMap((r) => r.data)
+
+    return NextResponse.json({
+        data:users,
+        total: users.length,
     });
-
-    const data = await res.json();
-
-    if (!res.ok) {
-      return NextResponse.json(
-        { error: data.error || "Failed to fetch users" },
-        { status: res.status }
-      );
-    }
-
-    return NextResponse.json(data);
   } catch (error) {
     return NextResponse.json(
       { error: "Failed to fetch users" },
